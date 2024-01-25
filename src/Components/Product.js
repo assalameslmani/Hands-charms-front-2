92e20f873f8aee54aa18e0836a467be3c5ddb483
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2024-01-25 10:05:49 +0200

    first commit

---
+++ src/Components/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import wishlist from "../images/heart.svg";
+import Product from "../data/Products";
+
+const Product = ({ id, name, description, price, image }) => {
+  return (
+    <div className="product">
+      <img src={image} alt={name} className='image-shop' />
+      <div className='like'>
+        <h3 className='name-shop'>{name}</h3>
+        <Link to="/wishlist"><img src={wishlist} className='like-img' /></Link> 
+      </div>
+      <p className='price-shop'>${price.toFixed(2)}</p>
+      <Link to="/cart" className='shop-btn'>Add to Cart</Link>
+    </div>
+  );
+};
+
+export default Product;
